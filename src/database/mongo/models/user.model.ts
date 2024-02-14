df95e4c4b5b04eb23636e5206c067cd244d7e85a
luismachaca <luismachaca@outlook.com.pe>
2024-02-14 09:52:54 -0500

    database schema and model was created

---
+++ src/database/mongo/models/user.model.ts
@@ -0,0 +1,42 @@
+import { Schema, model } from 'mongoose';
+
+const UserSchema = new Schema(
+    {
+        _id: { type: String, _id: false },
+        firstname: {
+            type: String,
+            minLength: 4,
+            maxLength: 30,
+            required: [true, 'Firstname is required']
+        },
+        lastname: {
+            type: String,
+            minLength: 5,
+            maxLength: 50,
+            required: [true, 'Lastname is required']
+        },
+        username: {
+            type: String,
+            minLength: 8,
+            maxLength: 30,
+            required: [true, 'Username is required'],
+            unique: true
+        },
+        photo: { type: String, required: false },
+        email: {
+            type: String,
+            required: [true, 'Email is required'],
+            unique: true
+        },
+        password: {
+            type: String,
+            minLength: 16,
+            required: [true, 'Password is required']
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const UserModel = model('User', UserSchema);
